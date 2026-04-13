47afe7d32296a1079d96effb16cabd4525e4c570
ashraf hassan <38661473+ashraflx71@users.noreply.github.com>
2026-04-13 11:47:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,5 @@
 
+st.sidebar.markdown("---")
+st.sidebar.markdown("### 💎 اكتشف المزيد")
+st.sidebar.write("هل تريد بناء تطبيق مشابه لمشروعك؟")
+st.sidebar.link_button("تواصل مع منصة Creative 2026", "https://ashrafhassan2026.blogspot.com")
